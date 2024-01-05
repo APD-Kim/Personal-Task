4f6d25734b9a54a5419ab853876a0d8b53a4b8aa
KimJaeYeon~ <popcon940620@gmail.com>
2024-01-05 19:57:30 +0900

    FINAL2

--- public/index.js
+++ public/index.js
@@ -5,101 +5,78 @@ document.addEventListener("DOMContentLoaded", () => {
   fetchMovieData();
   buildCard();
 });
+// const newSet = new Set();
+//size가 5를 넘어가는지
 
-console.log(arr);
-const numArr = [];
-console.log(numArr);
-let forCount = 5;
-for (let i = 0; i < forCount; i++) {
-  let randomNum = Math.floor(Math.random() * 20);
-  if (numArr.includes(randomNum)) {
-    forCount++;
-    continue;
-  }
-  numArr.push(randomNum);
-}
-
-const newSet = new Set();
-//size가 5를 넘어가는지 
-console.log(numArr);
-
-//최초 영화 리스트 생성
-let imgCnt = 0; //이미지가 몇개 생성되었는지 카운트
-fetchMovieData().then((movieContents) => {
-  for (let i = 0; i < 3; i++) {
-    document
-      .querySelectorAll(".main-box")[0]
-      .insertAdjacentHTML("beforeend", text(movieContents));
-    imgCnt += 5;
-  }
-});
+//이미지가 몇개 생성되었는지 카운트
 
 //영화 포스터를 배치하는 변수
-const text = function (movieContents) {
+let imgCnt = 0;
+const text = function (arr) {
   return `
     <div><img
-    src="https://image.tmdb.org/t/p/original${
-      movieContents[imgCnt].poster_path
-    }" id="${movieContents[imgCnt].id}"/></div>
+    src="https://image.tmdb.org/t/p/original${arr[imgCnt].poster_path}" id="${
+    arr[imgCnt].id
+  }"/></div>
     <div><img
     src="https://image.tmdb.org/t/p/original${
-      movieContents[imgCnt + 1].poster_path
-    }" id="${movieContents[imgCnt + 1].id}"/></div>
+      arr[imgCnt + 1].poster_path
+    }" id="${arr[imgCnt + 1].id}"/></div>
     <div><img
     src="https://image.tmdb.org/t/p/original${
-      movieContents[imgCnt + 2].poster_path
-    }" id="${movieContents[imgCnt + 2].id}"/></div>
+      arr[imgCnt + 2].poster_path
+    }" id="${arr[imgCnt + 2].id}"/></div>
     <div><img
     src="https://image.tmdb.org/t/p/original${
-      movieContents[imgCnt + 3].poster_path
-    }" id="${movieContents[imgCnt + 3].id}"/></div>
+      arr[imgCnt + 3].poster_path
+    }" id="${arr[imgCnt + 3].id}"/></div>
     <div><img
     src="https://image.tmdb.org/t/p/original${
-      movieContents[imgCnt + 4].poster_path
-    }" id="${movieContents[imgCnt + 4].id}"/></div>
+      arr[imgCnt + 4].poster_path
+    }" id="${arr[imgCnt + 4].id}"/></div>
   `;
 };
-//영화 리스트 추가 버튼
 
-// 영화 리스트 추가 생성
+//최초 영화 포스터 생성
+for (let i = 0; i < 3; i++) {
+  document
+    .querySelectorAll(".main-box")[0]
+    .insertAdjacentHTML("beforeend", text(arr));
+  imgCnt += 5;
+}
 
-fetchMovieData().then((movieContents) => {
-  const more = `<div class="more-btn">영화를 더 보고 싶다면 클릭!</div>`;
-  document.querySelectorAll(".more")[0].insertAdjacentHTML("beforeend", more);
+// 영화 리스트 추가 생성 및 버튼
+const more = `<div class="more-btn">영화를 더 보고 싶다면 클릭!</div>`;
+document.querySelectorAll(".more")[0].insertAdjacentHTML("beforeend", more);
 
-  document.querySelector(".more-btn").addEventListener("click", function () {
-    try {
-      document
-        .querySelectorAll(".main-box")[0]
-        .insertAdjacentHTML("beforeend", text(movieContents));
-      console.log(`추가적인 영화 등록 완료`);
-      imgCnt += 5;
-    } catch {
-      document.querySelector(".more-btn").textContent =
-        "더이상 영화가 없네요..";
-    }
-  });
+document.querySelector(".more-btn").addEventListener("click", function () {
+  try {
+    document
+      .querySelectorAll(".main-box")[0]
+      .insertAdjacentHTML("beforeend", text(arr));
+    imgCnt += 5;
+  } catch {
+    document.querySelector(".more-btn").textContent = "더이상 영화가 없네요..";
+  }
 });
 
 //추천카드의 버튼을 만드는 반복문
-const addCircleButton = `<button class="rec-btn"></button>`;
-const btnId = document.querySelector("#btn");
-const addButton = 5;
-
-for (let i = 0; i < addButton; i++) {
-  btnId.insertAdjacentHTML("beforeend", addCircleButton);
+for (let i = 0; i < 5; i++) {
+  document
+    .querySelector("#btn")
+    .insertAdjacentHTML("beforeend", `<button class="rec-btn"></button>`);
 }
 
-// 추천카드를 바꿔주는 함수
+//버튼 클릭 시 추천 영화 이미지 변경
 const recBtn = document.querySelectorAll(".rec-btn");
 const recContainer = document.querySelector(".rec-container");
 for (let i = 0; i < recBtn.length; i++) {
   recBtn[i].addEventListener("click", function () {
+    recContainer.style.transition = `all 1s`;
     recContainer.style.transform = `translateX(${-100 * i}vw)`;
   });
 }
-//모듈
-//foreach로 idValue에 맞는 id를 검색해서 일치하는 아이디가 있으면 해당 배열의 내용을 기반으로 모달창의 내용을 바꿔줘. 만약 없다면 catch해서 alret창에 내용이 아직 준비되지 않았다고 알려줘
+//이미지 클릭 시 ID 알림창
 const mainBox = document.querySelector(".main-box");
 mainBox.addEventListener("click", function (e) {
   if (e.target && e.target.matches("img")) {
@@ -107,10 +84,7 @@ mainBox.addEventListener("click", function (e) {
   }
 });
 
-//문자열을 넣고 검색했을때 movieContent라는 변수에 담긴 Array 안에 한글 title이나 영어 title중에 하나라도 맞는게 있다면 그 영화만 보여줘, 또한 영어로 검색했을 경우에는 lowerCase로 적용시켜줘
-
-//검색한 값에 포함되는 title의 포스터를 게시해줌
-//곽지민 , 우성종
+//검색기능
 document.querySelector("#search").addEventListener("click", function (e) {
   e.preventDefault();
   let value = document.querySelector("#input").value.toLowerCase();
--- src/function.js
+++ src/function.js
@@ -6,13 +6,12 @@ const options = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NzE2MjE1OWJkOGJhMGYxMTk5Zjc3ZDU0NDUxZGRhMCIsInN1YiI6IjY1OTM1NTdkNjUxZmNmNjAzZjhkZTJhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ekYKjL96tBuoevhvYdJBhCu94l-aWTXQb2e9auZVDOs",
   },
 };
-
+//API를 불러오는 함수
 export const fetchMovieData = async function () {
   const LocalData = localStorage.getItem("movieData");
   if (LocalData) {
     return JSON.parse(LocalData);
   } else {
-    //API를 불러오는 함수
     const response = await fetch(
       "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=11",
       options
@@ -23,25 +22,39 @@ export const fetchMovieData = async function () {
     return data.results;
   } // 영화 내부의 데이터를 반환
 };
+//localStorage에 있는 데이터를 전역변수로 저장
 export const arr = JSON.parse(localStorage.getItem(`movieData`));
 
+//겹치지 않는 랜덤한 정수 5개 출력
+const randomArr = [];
+let forCount = 5;
+for (let i = 0; i < forCount; i++) {
+  let randomNum = Math.floor(Math.random() * 20);
+  if (randomArr.includes(randomNum)) {
+    forCount++;
+    continue;
+  }
+  randomArr.push(randomNum);
+}
 
-
+//추천 영화 리스트 5장 생성
 export const buildCard = () => {
   const RECOMMEND_CARD_COUNT = 5;
   for (let i = 0; i < RECOMMEND_CARD_COUNT; i++) {
     const recCard = `
-      <div class="rec-card" style="background-image: linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ),url(https://image.tmdb.org/t/p/original${arr[i].backdrop_path})" id="${arr[i].id}">
+      <div class="rec-card" style="background-image: linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ),url(https://image.tmdb.org/t/p/original${
+        arr[randomArr[i]].backdrop_path
+      })" id="${arr[randomArr[i]].id}">
       <div>
         
         <div style="flex-direction: column">
           <div class="rec-content">
-            <p class="title"><span>${arr[i].title}</span>
+            <p class="title"><span>${arr[randomArr[i]].title}</span>
             <br>
-            <span>(${arr[i].original_title})</span>
+            <span>(${arr[randomArr[i]].original_title})</span>
             </p>
-            <p style="font-size:24px">평점 : ⭐${arr[i].vote_average}
-            <p class="overview">${arr[i].overview}</p>
+            <p style="font-size:24px">평점 : ⭐${arr[randomArr[i]].vote_average}
+            <p class="overview">${arr[randomArr[i]].overview}</p>
           </div>
         </div>
       </div>`;
