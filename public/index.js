8e43a6f29951d1925cd607bdd540cc238838221b
KimJaeYeon~ <popcon940620@gmail.com>
2024-01-07 01:20:11 +0900

    hide .env file

--- public/index.js
+++ public/index.js
@@ -1,4 +1,70 @@
-import { fetchMovieData, buildCard, arr } from "./function.js";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "process.env.ACCESS_TOKEN",
+  },
+};
+
+console.log(options.headers.Authorization);
+
+//API를 불러오는 함수
+const fetchMovieData = async function () {
+  const LocalData = localStorage.getItem("movieData");
+  if (LocalData) {
+    return JSON.parse(LocalData);
+  } else {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=11",
+      options
+    );
+    const data = await response.json();
+    localStorage.setItem("movieData", JSON.stringify(data.results));
+    console.log(data);
+    return data.results;
+  } // 영화 내부의 데이터를 반환
+};
+//localStorage에 있는 데이터를 전역변수로 저장
+const arr = JSON.parse(localStorage.getItem(`movieData`));
+
+//겹치지 않는 랜덤한 정수 5개 출력
+const randomArr = [];
+let forCount = 5; //6
+for (let i = 0; i < forCount; i++) {
+  let randomNum = Math.floor(Math.random() * 20);
+  if (randomArr.includes(randomNum)) {
+    forCount++;
+    continue;
+  }
+  randomArr.push(randomNum);
+}
+
+//추천 영화 리스트 5장 생성
+const buildCard = () => {
+  const RECOMMEND_CARD_COUNT = 5;
+  for (let i = 0; i < RECOMMEND_CARD_COUNT; i++) {
+    const recCard = `
+      <div class="rec-card" style="background-image: linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ),url(https://image.tmdb.org/t/p/original${
+        arr[randomArr[i]].backdrop_path
+      })" id="${arr[randomArr[i]].id}">
+      <div>
+        
+        <div style="flex-direction: column">
+          <div class="rec-content">
+            <p class="title"><span>${arr[randomArr[i]].title}</span>
+            <br>
+            <span>(${arr[randomArr[i]].original_title})</span>
+            </p>
+            <p style="font-size:24px">평점 : ⭐${arr[randomArr[i]].vote_average}
+            <p class="overview">${arr[randomArr[i]].overview}</p>
+          </div>
+        </div>
+      </div>`;
+    document
+      .querySelectorAll(".rec-container")[0]
+      .insertAdjacentHTML("beforeend", recCard);
+  }
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   // 페이지가 로드될 때 fetchMovieData 함수 실행
@@ -78,7 +144,7 @@ mainBox.addEventListener("click", function (e) {
   console.log(e.target);
   if (e.target && e.target.matches("img")) {
     alert(`ID : ${e.target.id}`);
-  };
+  }
 });
 
 //검색기능
@@ -104,4 +170,3 @@ document.querySelector("#search").addEventListener("click", function (e) {
 //css스타일에서 display를 사용하려고 했으나, 그냥 이렇게 해보고싶어서 했습니다.
 
 //grid column
-
